ec40f24bf10c47ba6e00eca5af00131e1d03408a
Yangzhengdong1 <67670781+Yangzhengdong1@users.noreply.github.com>
2023-12-21 11:07:30 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -11,7 +11,6 @@ module.exports = {
     proxy: {
       '/httpProxy': {
         target: 'http://192.168.18.170:8889', // 开发环境后台接口域名
-        // target: 'http://111.229.233.21:8083', // 测试环境后台接口域名
         changeOrigin: true, //是否跨域
         pathRewrite: {
           '^/httpProxy': ''
